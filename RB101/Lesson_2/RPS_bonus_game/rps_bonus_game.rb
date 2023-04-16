e0474574b05ad36426c77273922ac4565f943b79
Ubuntu <ubuntu@ip-172-31-95-129.ec2.internal>
2023-04-16 17:13:10 +0000

    Updated

--- RB101/Lesson_2/RPS_bonus_game/rps_bonus_game.rb
+++ RB101/Lesson_2/RPS_bonus_game/rps_bonus_game.rb
@@ -1,5 +1,7 @@
 require 'yaml'
 MESSAGES = YAML.load_file('rps_bonus_messages.yaml')
+SCORE_TO_WIN = 3
+RESET = 0
 
 CHOICES = {
   'r' => 'rock',
@@ -15,13 +17,14 @@ WIN_CONDITION = [['sc', 'p'], ['p', 'r'], ['r', 'l'],
 def prompt(key, **options)
   message = format(MESSAGES[key], **options)
   puts "=> #{message}"
+  puts
 end
 
 def valid?(string)
-  CHOICES.key?(string)
+  CHOICES.key?(string) || CHOICES.value?(string)
 end
 
-def win?(user_choice, computer_choice)
+def who_won(user_choice, computer_choice)
   if user_choice == computer_choice
     'tie'
   elsif WIN_CONDITION.include?([user_choice, computer_choice])
@@ -31,8 +34,26 @@ def win?(user_choice, computer_choice)
   end
 end
 
-prompt('welcome')
+def user_choice_display(user_choice)
+  if user_choice.length > 2
+    if user_choice == 'scissors'
+      'sc'
+    elsif user_choice == 'spock'
+      'sp'
+    else
+      user_choice[0]
+    end
+  else
+    user_choice
+  end
+end
 
+system "clear"
+
+prompt('welcome')
+prompt('grand_winner_instruction')
+sleep(3)
+system "clear"
 user_score = 0
 computer_score = 0
 user_choice = ''
@@ -43,42 +64,46 @@ while repeat
     computer_choice = ['p', 'r', 'sc', 'sp', 'l'].sample()
     loop do
       prompt('choice')
-      user_choice = gets.chomp.downcase()
+      user_choice = gets.chomp.downcase().gsub(/[' "]/, '')
       break if valid?(user_choice)
       prompt('not_valid')
     end
 
-    result = win?(user_choice, computer_choice)
+    result = who_won(user_choice_display(user_choice), computer_choice)
 
     if result == 'tie'
       prompt('tie')
     elsif result == 'user'
       user_score += 1
-      prompt('won', winner: "You", user_choice: CHOICES[user_choice],
+      prompt('won', winner: "You", 
+                    user_choice: CHOICES[user_choice_display(user_choice)],
                     computer_choice: CHOICES[computer_choice])
     else
       computer_score += 1
-      prompt('won', winner: "Computer", user_choice: CHOICES[user_choice],
+      prompt('won', winner: "Computer", 
+                    user_choice: CHOICES[user_choice_display(user_choice)],
                     computer_choice: CHOICES[computer_choice])
     end
 
     prompt('score', user_score: user_score, computer_score: computer_score)
 
-    if user_score == 3
+    if user_score == SCORE_TO_WIN
       grand_winner = "you"
       break
-    elsif computer_score == 3
+    elsif computer_score == SCORE_TO_WIN
       grand_winner = "computer"
       break
     end
+    sleep(2)
+    system "clear"
   end
-
+  
   prompt('grand_winner', grand_winner: grand_winner)
 
   prompt('repeat')
   repeat = gets.chomp.downcase().gsub(/[' "]/, '').start_with?('y')
   if repeat
-    user_score = computer_score = 0
+    user_score = computer_score = RESET
   else
     break
   end
