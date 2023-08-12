c36e73e6d1433c5a891e4e8505eac4b03c2e5b2b
StartZer0 <59214167+StartZer0@users.noreply.github.com>
2023-08-12 19:31:29 -0400

    Major update rps_bonus.rb
    Added two modules FindWinner and Displayable. 
    Refactored RPSGame class.
    Changed logic to determine winner
    Eliminated object's classes and grouped win conditions into constant Hash in class `Move` 
    Refactored several method, including `game_flow` method
    And other minor changes to the code was implemented.

--- RB120/rps_bonus.rb
+++ RB120/rps_bonus.rb
@@ -1,115 +1,65 @@
 # This class will be used to track total score and determine grand winner
 class Score
-  attr_accessor :score
+  attr_accessor :score, :score_to_win
 
-  SCORE_TO_WIN = 10
   RESET = 0
-  def initialize
+
+  def initialize(score_to_win = 3)
+    @score_to_win = score_to_win
     @score = 0
   end
 
   def check_grand_score
-    @score == SCORE_TO_WIN
+    @score == @score_to_win
   end
 
-  def reset_score
+  def reset
     @score = RESET
   end
 
-  def increment_score
+  def increment
     @score += 1
   end
 end
 
-class MoveType
-  attr_accessor :value
-
-  def win_condition(win_condition, choice)
-    win_condition.include?(choice)
-  end
-end
-
-class Paper < MoveType
-  WIN_CONDITION = ['rock', 'spock']
-
-  def initialize
-    super()
-    @value = 'paper'
-  end
-end
-
-class Scissors < MoveType
-  WIN_CONDITION = ['paper', 'lizard']
-
-  def initialize
-    super()
-    @value = 'scissors'
-  end
-end
-
-class Lizard < MoveType
-  WIN_CONDITION = ['paper', 'spock']
-
-  def initialize
-    super()
-    @value = 'lizard'
-  end
-end
-
-class Spock < MoveType
-  WIN_CONDITION = ['rock', 'scissors']
-
-  def initialize
-    super()
-    @value = 'spock'
-  end
-end
-
-class Rock < MoveType
-  WIN_CONDITION = ['lizard', 'scissors']
-
-  def initialize
-    super()
-    @value = 'rock'
-  end
-end
-
 class Move
-  VALUES = %w(rock paper lizard spock scissors)
-  OBJECTS = [Rock.new, Paper.new, Scissors.new, Lizard.new, Spock.new]
-  attr_accessor :moves
+  attr_accessor :value, :moves
 
   def initialize
     @moves = {}
   end
 
+  WIN_CONDITIONS = {
+    'rock' => ['lizard', 'scissors'],
+    'paper' => ['rock', 'spock'],
+    'scissors' => ['paper', 'lizard'],
+    'lizard' => ['paper', 'spock'],
+    'spock' => ['rock', 'scissors']
+  }
+
+  VALUES = WIN_CONDITIONS.keys
+
+  def win_condition(choice)
+    WIN_CONDITIONS[value].include?(choice)
+  end
+
   # Push all moves into hash to track players moves
+  def track_move(player_name, move)
+    @moves[player_name] ||= []
+    @moves[player_name].push(move)
+  end
+
   def track_moves(human_move, computer_move, human, computer)
-    @moves[human.name] ||= []
-    @moves[human.name].push(human_move)
-
-    @moves[computer.name] ||= []
-    @moves[computer.name].push(computer_move)
+    track_move(human.name, human_move)
+    track_move(computer.name, computer_move)
   end
+end
 
+module FindWinner
   def human_win?(human_move, computer_move)
-    OBJECTS.any? do |object|
-      object.win_condition(object.class::WIN_CONDITION,
-                           computer_move) && object.value == human_move
-    end
-  end
-
-  def display_winner(winner, human, computer)
-    case winner
-    when :human
-      human.increment_score
-      puts "=> #{human.name} won!"
-    when :computer
-      computer.increment_score
-      puts "=> #{computer.name} won!"
-    else
-      puts "=> It's a tie!"
-    end
+    human_move_type = Move.new
+    human_move_type.value = human_move
+    human_move_type.win_condition(computer_move)
   end
 
   def determine_winner(human, computer)
@@ -123,12 +73,16 @@ class Move
     else
       display_winner(:computer, human, computer)
     end
-    track_moves(human_move, computer_move, human, computer)
+    moves.track_moves(human_move, computer_move, human, computer)
   end
 end
 
-class Player < Score
-  attr_accessor :name, :move
+class Player
+  attr_accessor :name, :move, :score
+
+  def initialize
+    @score = Score.new
+  end
 end
 
 class Human < Player
@@ -141,7 +95,7 @@ class Human < Player
     name = ''
     loop do
       puts '=> Please, enter your name: '
-      name = gets.chomp
+      name = gets.chomp.strip
       break unless name.empty?
 
       puts 'Sorry, you need to enter your name: '
@@ -149,11 +103,23 @@ class Human < Player
     self.name = name
   end
 
+  def identify_move(choice)
+    choices_map = {
+      "r" => "rock",
+      "p" => "paper",
+      "l" => "lizard",
+      "sp" => "spock",
+      "s" => "scissors"
+    }
+    choices_map[choice]
+  end
+
   def choose
     choice = nil
     loop do
-      puts '=> Please, choose rock, paper, lizard, spock or scissors: '
+      puts '=> Please, choose (r)ock, (p)aper, (l)izard, (sp)ock or (s)cissors:'
       choice = gets.chomp.downcase
+      choice = identify_move(choice) if choice.size == 1
       break if Move::VALUES.include?(choice)
 
       puts 'Sorry, invalid choice.'
@@ -164,6 +130,7 @@ end
 
 class Computer < Player
   NAMES = ['Android', 'Hal', 'R2D2']
+
   def initialize
     set_name
     super
@@ -181,26 +148,34 @@ class Computer < Player
 
   def favored_choices
     if name == 'Hal'
-      return (['scissors'] * 4) + ['rock'] + (['lizard',
-                                               'spock'] * 2)
+      return (['scissors'] * 4) + ['rock'] + (['lizard', 'spock'] * 2)
     end
     return ['rock'] if name == 'R2D2'
     Move::VALUES
   end
 end
 
-class RPSGame
-  attr_accessor :human, :computer, :moves
+module Displayable
+  def choose_number_of_rounds
+    answer = ''
+    loop do
+      puts '=> How many rounds would you like to play?'
+      answer = gets.chomp.strip
+      break if answer.to_i.to_s == answer && answer.to_i > 0
 
-  def initialize
-    @human = Human.new
-    @computer = Computer.new
-    @moves = Move.new
+      puts 'Sorry, you need to enter a valid number higher than 0'
+    end
+    display_rounds_message(answer)
+  end
+
+  def display_rounds_message(answer)
+    human.score.score_to_win = answer.to_i
+    computer.score.score_to_win = answer.to_i
+    puts "Win #{human.score.score_to_win} rounds to become a grand winner"
   end
 
   def display_welcome_message
     puts "=> Welcome #{human.name} to Rock, Paper, Lizard, Spock, Scissors!"
-    puts "You need to win 10 times to become a grand winner"
   end
 
   def display_choices
@@ -208,15 +183,40 @@ class RPSGame
     puts "=> #{computer.name} chose #{computer.move}"
   end
 
+  def display_winner(winner, human, computer)
+    case winner
+    when :human
+      human.score.increment
+      puts "=> #{human.name} won this round!"
+    when :computer
+      computer.score.increment
+      puts "=> #{computer.name} won this round!"
+    else
+      puts "=> It's a tie!"
+    end
+  end
+
   def display_score
-    puts "=> #{human.name} score: #{human.score}"
-    puts "=> #{computer.name} score: #{computer.score}"
+    puts "=> #{human.name} score: #{human.score.score}"
+    puts "=> #{computer.name} score: #{computer.score.score}"
   end
 
   def display_moves
     moves.moves.each do |player, moves|
       puts "=> #{player} moves: #{moves.join(' ')} "
     end
+  end
+
+  def next_round_message(round, duration = 2.5)
+    message = "Be Ready for Round #{round}..."
+    interval = duration.to_f / message.length
+
+    message.each_char do |char|
+      print char
+      sleep(interval)
+    end
+
+    puts
   end
 
   def display_players_stats
@@ -229,46 +229,65 @@ class RPSGame
     puts 'Thanks for playing Rock, Paper, Lizard, Spock, Scissors. Good bye!'
   end
 
-  def play_again?
-    answer = ''
-    loop do
-      puts '=> Would you like to play again? Enter: y or n'
-      answer = gets.chomp.downcase
-      break if ['y', 'n'].include?(answer)
+  def display_grand_winner(player)
+    puts "=> #{player.name} is a grand winner!"
+    computer.score.reset
+    human.score.reset
+    moves.moves = {}
+  end
+end
 
-      puts 'Sorry, must be y or n'
-    end
-    return true if answer == 'y'
+class RPSGame
+  include Displayable
+  include FindWinner
 
-    false
+  attr_accessor :human, :computer, :moves, :score
+
+  def initialize
+    @human = Human.new
+    @computer = Computer.new
+    @moves = Move.new
+    @score = Score.new
   end
 
   def grand_winner
-    return computer if computer.check_grand_score
-    return human if human.check_grand_score
+    return computer if computer.score.check_grand_score
+    return human if human.score.check_grand_score
   end
 
-  def display_grand_winner(player)
-    puts "=> #{player.name} is a grand winner!"
-    computer.reset_score
-    human.reset_score
-    moves.moves = {}
+  def round_flow
+    human.choose
+    computer.choose
+    determine_winner(human, computer)
+    display_players_stats
+  end
+
+  def prepare_next_round(round)
+    next_round_message(round)
+    system 'clear'
+  end
+
+  def check_and_display_grand_winner
+    if grand_winner
+      display_grand_winner(grand_winner)
+      return true
+    end
+    false
   end
 
   def game_flow
+    round = 1
     loop do
-      human.choose
-      computer.choose
-      moves.determine_winner(human, computer)
-      display_players_stats
-      display_grand_winner(grand_winner) if grand_winner
-      break unless play_again?
-      system 'clear'
+      prepare_next_round(round)
+      round_flow
+      break if check_and_display_grand_winner
+      round += 1
     end
   end
 
   def play
     display_welcome_message
+    choose_number_of_rounds
     game_flow
     display_goodbye_message
   end
